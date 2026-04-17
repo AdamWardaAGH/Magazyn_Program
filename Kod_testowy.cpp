96d4e5a2389086124fefeeb772b86423d64d075a
c-Grox <kubwitt@student.agh.edu.pl>
2026-04-17 17:42:40 +0200

    woah technology

--- Kod_testowy.cpp
+++ Kod_testowy.cpp
@@ -5,6 +5,7 @@
 int main(){
 setlocale(LC_ALL, "pl_PL.UTF-8");
     std::cout << "Kod testowy\nUsunąć kiedy repozytorium będzie już miało zawartość" <<std::endl;
+    std::cout << "Zmodyfikowałem code i jestem cool ~ JW\n";
 
     return 0;
 }
