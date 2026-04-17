b0818714ba617726f2f637a415bb2f3b1b9169b2
AdamWardaAGH <adamwarda@student.agh.edu.pl>
2026-04-17 17:34:39 +0200

    first commit

---
+++ Kod_testowy.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<clocale>
+// aby było koszernie to możmy nie używać stdnamespace by nie było haosu
+
+int main(){
+setlocale(LC_ALL, "pl_PL.UTF-8");
+    std::cout << "Kod testowy\nUsunąć kiedy repozytorium będzie już miało zawartość" <<std::endl;
+
+    return 0;
+}
